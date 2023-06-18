d904bacb8916173c4978c193e4d56349694398b4
Lonkand19 <o.kandelaki@outlook.com>
2023-06-18 16:01:28 +0400

    Davadedsad

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ let container = document.getElementById('container');
 let container2 = document.getElementById('container1');
 let container3 = document.getElementById('container2');
 let container4 = document.getElementById('container3');
+let block = document.querySelector('.text');
 
-container.addEventListener('click', () => {
+
+container.addEventListener('mouseover', () => {
     document.querySelector('.text').style.display = 'block';
     document.querySelector('.text1').style.display = 'none';
     document.querySelector('.text2').style.display = 'none';
     document.querySelector('.text3').style.display = 'none';
     container.style.boxShadow = 'inset 0 0 60px whitesmoke, inset 20px 0 80px #f0f, inset -20px 0 80px #0ff, inset 20px 0 300px #f0f, inset -20px 0 300px #0ff, 0 0 50px #fff, -10px 0 80px #f0f, 10px 0 80px #0ff';
-    container.style.transform = 'translateY(-100px)';
+    container.style.transform = 'translateY(-70px)';
     container2.style.transform = 'translateY(100px)';
     container3.style.transform = 'translateY(100px)';
     container4.style.transform = 'translateY(100px)';
